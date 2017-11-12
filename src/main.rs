974ee77d96844aad2a820c351f845c5c7b2cd0e2
Admexter <mypancakes@wp.pl>
2017-11-12 20:15:05 +0100

    day 2

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,34 @@
 #![allow(dead_code)]
 
 extern crate rand;
+extern crate piston_window;
 mod postac;
 
+use std::collections::{HashMap, HashSet};
+
 use rand::*;
+use piston_window::*;
 
 use postac::*;
 
-use std::collections::HashMap;
-
 fn main() {
-    let postac = Postac {
-        imie: "Ao Dojo".into(),
-        klasa: Klasa {
-            nazwa: "Paladyn".into(),
-            kosc_wytrz: 10,
-        },
-        rasa: Rasa {
-            nazwa: "Człowiek".into(),
-        },
-        charakter: Charakter::PrawyDobry,
-        bostwo: "Bahamut".into(),
-        wyglad: Wyglad {
+    let mut window: PistonWindow = WindowSettings::new("D&D", [800, 600])
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+    let mut glyphs = Glyphs::new(
+        "assets/FiraSans-Regular.ttf",
+        window.factory.clone(),
+        TextureSettings::new(),
+    ).unwrap();
+    let postac = Postac::new(
+        "Ao Dojo",
+        (0, 0, 12, 0),
+        Klasa::new("Paladyn".into(), 10, vec![1]),
+        Rasa { nazwa: "Dragonborn".into() },
+        Charakter::PrawyDobry,
+        "Bahamut",
+        Wyglad {
             rozmiar: Rozmiar::Sredni,
             wiek: 30,
             plec: Plec::Mezczyzna,
@@ -31,18 +38,68 @@ fn main() {
             wlosy: None,
             skora: "granatowe luski".into(),
         },
-        podstawowe_atrybuty: Atrybuty::podst(),
-        zycie: 10,
-        stluczenia: 0,
-        klasa_pancerza: 10,
-        umiejetnosci: HashMap::new(),
-    };
+        Atrybuty::podst(),
+        HashMap::new(),
+        HashSet::new(),
+    );
     println!("Witaj, {:?}!", postac);
     println!("Twoja siła to: {}", postac.podstawowe_atrybuty.sila);
-    println!("Twój modyfikator siły to: {}", modyfikator(postac.podstawowe_atrybuty).sila);
+    println!(
+        "Twój modyfikator siły to: {}",
+        modyfikator(postac.podstawowe_atrybuty).sila
+    );
     let mut rng = thread_rng();
     for i in 0..4 {
         let rzut = rng.gen_range::<u8>(1, 21);
         println!("Rzut {}.: {}", i + 1, rzut);
+    }
+    let mut new_game_pressed = false;
+    let mut left_pressed = false;
+
+    while let Some(e) = window.next() {
+        e.press(|b| {
+            match b {
+                Button::Mouse(MouseButton::Left) => left_pressed = true,
+                Button::Mouse(MouseButton::Right) => println!("zycze milego dnia, takze spada"),
+                _ => (),
+            }
+        });
+        e.release(|b| {
+            match b {
+                Button::Mouse(MouseButton::Left) => left_pressed = false,
+                _ => (),
+            }
+        });
+        e.mouse_cursor(|x, y| {
+            let (w, h) = window.draw_size().into();
+            let (w, h) = (w as f64, h as f64);
+            new_game_pressed = 
+                left_pressed && 
+                x > (w / 4.) && x < (w * 3. / 4.) &&
+                y > (h / 4.) && y < (h * 2. / 4.);
+        });
+        window.draw_2d(&e, |c, g| {
+            let size = c.viewport.unwrap().window_size;
+            let (x, y) = (size[0] as f64, size[1] as f64);
+            clear([0., 0., 0., 1.], g);
+            let color = if !new_game_pressed {
+                [0., 0., 0.7, 1.]
+            } else {
+                [0., 0.4, 0.4, 1.]
+            };
+            rectangle(
+                color,
+                [x / 4., y / 4., x / 2., y / 4.],
+                c.transform,
+                g,
+            );
+            text::Text::new_color([1., 1., 1., 1.], (x / 30.) as _).draw(
+                "New game",
+                &mut glyphs,
+                &c.draw_state,
+                c.transform.trans(x * 3. / 8., y * 3. / 8.),
+                g
+            ).unwrap();
+        });
     }
 }
--- src/postac.rs
+++ src/postac.rs
@@ -1,15 +1,28 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 #[derive(Debug)]
 pub struct Klasa {
     pub nazwa: String,
     pub kosc_wytrz: u8,
+    pub bazowa_premia_do_ataku: Vec<u8>,
+}
+
+impl Klasa {
+    pub fn new(nazwa: String, kosc_wytrz: u8, bazowa_premia_do_ataku: Vec<u8>) -> Self {
+        Klasa {
+            nazwa,
+            kosc_wytrz,
+            bazowa_premia_do_ataku,
+        }
+    }
 }
 
 #[derive(Debug)]
 pub struct Rasa {
     pub nazwa: String,
 }
+
+
 
 #[derive(Debug)]
 pub enum Rozmiar {
@@ -58,11 +71,11 @@ pub enum Charakter {
 #[derive(Debug)]
 pub struct Atrybuty {
     pub sila: i8,
-    pub zrecznosc: i8,
-    pub budowa: i8,
-    pub inteligencja: i8,
-    pub roztropnosc: i8,
-    pub charyzma: i8,
+    zrecznosc: i8,
+    budowa: i8,
+    inteligencja: i8,
+    roztropnosc: i8,
+    charyzma: i8,
 }
 
 impl Atrybuty {
@@ -89,18 +102,38 @@ pub enum Atrybut {
 }
 
 #[derive(Debug)]
+pub struct Korzysc;
+
+type Rzut = (u8, u8);
+
+#[derive(Debug)]
 pub struct Postac {
-    pub imie: String,
-    pub klasa: Klasa,
-    pub rasa: Rasa,
-    pub charakter: Charakter,
-    pub bostwo: String,
-    pub wyglad: Wyglad,
+    imie: String,
+    poziom: u8,
+    doswiadczenie: u32,
+    kasa: (u32, u32, u32, u32),
+    klasa: Klasa,
+    rasa: Rasa,
+    charakter: Charakter,
+    bostwo: String,
+    wyglad: Wyglad,
     pub podstawowe_atrybuty: Atrybuty,
-    pub zycie: u8,
-    pub stluczenia: u8,
-    pub klasa_pancerza: u8,
-    pub umiejetnosci: HashMap<&'static str, Atrybut>,
+    zycie_max: u16,
+    zycie: f32,
+    stluczenia: u8,
+    klasa_pancerza: u8,
+    umiejetnosci: HashMap<String, Korzysc>,
+    atuty: HashMap<String, Korzysc>,
+    inicjatywa: u8,
+    rzuty_obronne: (u8, u8, u8),
+    zwarcie: u8,
+    jezyki: HashSet<String>,
+    //nazwa, waga, obrazenia, krytyk, zasieg, specjalne, opis
+    bronie: HashMap<String, (f32, Rzut, (u8, u8), u8, Korzysc, String)>,
+    //nazwa, waga, premia do KP, specjalne, opis
+    wyposazenie: HashMap<String, (f32, u8, Korzysc, String)>,
+    //nazwa, waga, ilosc
+    ekwipunek_schowany: HashMap<String, (f32, u16)>,
 }
 
 pub fn modyfikator(atrybuty: Atrybuty) -> Atrybuty {
@@ -111,5 +144,49 @@ pub fn modyfikator(atrybuty: Atrybuty) -> Atrybuty {
         inteligencja: (atrybuty.inteligencja / 2).saturating_sub(5),
         roztropnosc: (atrybuty.roztropnosc / 2).saturating_sub(5),
         charyzma: (atrybuty.charyzma / 2).saturating_sub(5),
+    }
+}
+
+impl Postac {
+    pub fn new<S: Into<String>>(
+        imie: S,
+        kasa: (u32, u32, u32, u32),
+        klasa: Klasa,
+        rasa: Rasa,
+        charakter: Charakter,
+        bostwo: S,
+        wyglad: Wyglad,
+        podstawowe_atrybuty: Atrybuty,
+        umiejetnosci: HashMap<String, Korzysc>,
+        mut jezyki: HashSet<String>,
+    ) -> Self {
+        Postac {
+            imie: imie.into(),
+            poziom: 1,
+            doswiadczenie: 0,
+            kasa,
+            klasa,
+            rasa,
+            charakter,
+            bostwo: bostwo.into(),
+            wyglad,
+            podstawowe_atrybuty,
+            zycie_max: 0,
+            zycie: 0.0,
+            stluczenia: 0,
+            klasa_pancerza: 10,
+            umiejetnosci,
+            atuty: HashMap::new(),
+            inicjatywa: 0,
+            rzuty_obronne: (0, 0, 0),
+            zwarcie: 0,
+            jezyki: {
+                jezyki.insert("Wspólny".into());
+                jezyki
+            },
+            bronie: HashMap::new(),
+            wyposazenie: HashMap::new(),
+            ekwipunek_schowany: HashMap::new(),
+        }
     }
 }
